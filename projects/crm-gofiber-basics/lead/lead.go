bad5a3f7a19ce58c7642087a2bb7d7fa51008f1d
Rajesh kumar S R <rajesh.kumar@colortokens.com>
2024-01-09 12:50:06 +0530

    Go Project CRM GoFiber, SQLITe db

---
+++ projects/crm-gofiber-basics/lead/lead.go
@@ -0,0 +1,59 @@
+package lead
+
+
+import (
+	"github.com/rajuuu1992/crm-gofiber-basics/database"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gofiber/fiber"
+)
+
+type struct Lead {
+	gorm.Model
+	Name string `json: "Name"`
+	Phone int   `json: "Phone"`
+	Email string `json: "email"`
+}
+
+func GetLeads(c *fiber.Ctx) error {
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+}
+
+func GetLead(c *fiber.Ctx) error {
+	res := utils.CopyString(c.Params("id"))
+
+	var getLead Lead
+	db.Find(&getLead, id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx) error {
+	params := c.Params("id")
+
+	lead := new(Lead)
+
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	database.DBConn.Create(&lead)
+	c.JSON(lead)
+}
+
+func DeleteLead(c *fiber.Ctx) error {
+	params := c.Params("id")
+
+	// var lead Lead
+	// db.Where("ID=?", ID).Delete(lead)
+
+	var lead Lead
+	db.First(&lead, id)
+	if lead.Name == "" {
+		c.Status(500).Send("No lead found with Id")
+		return
+	}
+	db.Delete(&lead)
+	c.Send("Lead deleted successfully")
+}
---
+++ projects/crm-gofiber-basics/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/rajuuu1992/crm-gofiber-basics/database"
+	"github.com/rajuuu1992/crm-gofiber-basics/lead"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+
+	var err error
+	database.DBConn, err := gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printfln("DB Connect success")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated")
+}
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(7777)
+
+	defer database.DBConn.Close()
+
+}
